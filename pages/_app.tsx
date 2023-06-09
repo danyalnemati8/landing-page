08470ef95d3b6d1232df0052dec2f3ae9ae4e8f8
danyalnemati8 <dnemati@csu.fullerton.edu>
2023-06-08 20:24:17 -0700

    tslandingpage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,15 @@
-import '@/styles/globals.css'
+import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { NextUIProvider, createTheme } from '@nextui-org/react'
+import { NextUIProvider, createTheme} from '@nextui-org/react';
 
-const darkTheme = createTheme({type: 'dark'})
+const darkTheme = createTheme({type: "dark"});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return(
-    <NextUIProvider theme={darkTheme} >
+  return (
+    <NextUIProvider theme={darkTheme}>
       <Component {...pageProps} />
     </NextUIProvider>
-  ) 
+  )
 }
 
 export default MyApp
